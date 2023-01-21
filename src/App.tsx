74b6578086e1bf375ddd8e19fe7f368b159de3c1
chocomintw <ienjoychocomint@gmail.com>
2023-01-21 14:29:15 +0100

    finished new line logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import {useState} from 'react'
+import {useState} from 'react';
 
 export default function App() {
-  const [chatlog, setChatlog] = useState('')
+  const [chatlog, setChatlog] = useState('');
 
   function handleChatlog(chatlog: string): void {
-    return setChatlog(chatlog)
+    return setChatlog(chatlog);
   }
 
   function formatChatlog(chatlog: string) {
-    return chatlog.split('\n').map(str => <p>{str}</p>)
+    return chatlog
+      .split('\n')
+      .map(str => <div className="outputChatlog">{str}</div>);
   }
 
   return (
-    <div className="App">
+    <>
       <textarea value={chatlog} onChange={e => handleChatlog(e.target.value)} />
       {formatChatlog(chatlog)}
-    </div>
-  )
+    </>
+  );
 }
