495bd8e8dfbcd5ae0ce08836af671a06b7b36c7e
chocomintw <ienjoychocomint@gmail.com>
2023-05-04 15:52:39 +0200

    no todo now

--- src/Components/HandleChatlog.tsx
+++ src/Components/HandleChatlog.tsx
@@ -77,17 +77,14 @@ export default function HandleChatlog() {
           : '':
           return <div className="outputChatlog give">{str}</div>;
 
-        // TODO: character kill
         case str.startsWith('[Character kill]') ? str: '':
           const newStr = str.split(" ")
-
           return <>
             <div className='outputChatlog'>
               <span className='ckBlue'>{"[Character kill] "}</span>
               <span className='outputChatlog ckRed'>{newStr[2]} {newStr[3]} {newStr[4]} {newStr[5]} {newStr[6]}</span>
             </div>
           </>
-
 
         // default message in chat
         default:
