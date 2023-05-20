a0ebe7e122668ee03c6bdcb40632db5aecc16bd4
chocomintw <ienjoychocomint@gmail.com>
2023-05-20 14:03:56 +0200

    you can now increase and decrease font size

--- src/Components/HandleChatlog.tsx
+++ src/Components/HandleChatlog.tsx
@@ -4,6 +4,7 @@ import fileDownload from "js-file-download";
 
 export default function HandleChatlog() {
   const [chatlog, setChatlog] = useState('');
+  const [fontsize, setFontsize] = useState(15)
 
   function stateChatlog(chatlog: string): void {
     return setChatlog(chatlog);
@@ -25,70 +26,100 @@ export default function HandleChatlog() {
       switch (str) {
         // radio (primary slot)
         case str.toUpperCase().includes('S: 1 | CH:') ? str : '':
-          return <div className="outputChatlog radioPrimary">{str}</div>;
+          return <div className="outputChatlog radioPrimary" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // radio (secondary slot)
         case str.toUpperCase().includes('| CH:') ? str : '':
-          return <div className="outputChatlog radioSecondary">{str}</div>;
+          return <div className="outputChatlog radioSecondary" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         case str.startsWith('((') ? str : '':
-          return <div className="outputChatlog ooc">{str}</div>;
+          return <div className="outputChatlog ooc" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // /me
         case str.startsWith('*') || str.startsWith('>') ? str : '':
-          return <div className="outputChatlog me">{str}</div>;
+          return <div className="outputChatlog me" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // [low]
         case str.toLowerCase().includes('says [low]') ? str : '':
-          return <div className="outputChatlog low">{str}</div>;
+          return <div className="outputChatlog low" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // whispers
         case str.toLowerCase().includes('whispers:') ? str : '':
-          return <div className="outputChatlog whisper">{str}</div>;
+          return <div className="outputChatlog whisper" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // atc radio
         case str.toUpperCase().startsWith('[CH: ATC]') ? str : '':
-          return <div className="outputChatlog atc">{str}</div>;
+          return <div className="outputChatlog atc" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // atc controller
         case str.toUpperCase().startsWith('[CH: ATC - AIR TRAFFIC CONTROLLER]')
           ? str
           : '':
-          return <div className="outputChatlog atcController">{str}</div>;
+          return <div className="outputChatlog atcController" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // vts
         case str.toUpperCase().startsWith('[CH: VTS') ? str : '':
-          return <div className="outputChatlog vts">{str}</div>;
+          return <div className="outputChatlog vts" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // dep
         case str.includes('->') ? str : '':
-          return <div className="outputChatlog dep">{str}</div>;
+          return <div className="outputChatlog dep" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // money
         case str.toLowerCase().includes('paid you $') ||
         str.toLowerCase().includes('you paid $')
           ? str
           : '':
-          return <div className="outputChatlog money">{str}</div>;
+          return <div className="outputChatlog money" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         // give/receive
         case str.startsWith('You received') || str.startsWith('You gave')
           ? str
           : '':
-          return <div className="outputChatlog give">{str}</div>;
+          return <div className="outputChatlog give" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
 
         case str.startsWith('[Character kill]') ? str: '':
           const newStr = str.split(" ")
           return <>
             <div className='outputChatlog'>
-              <span className='ckBlue'>{"[Character kill] "}</span>
-              <span className='outputChatlog ckRed'>{newStr[2]} {newStr[3]} {newStr[4]} {newStr[5]} {newStr[6]}</span>
+              <span className='ckBlue' style={{
+            fontSize: `${fontsize}px`
+          }}>{"[Character kill] "}</span>
+              <span className='outputChatlog ckRed' style={{
+            fontSize: `${fontsize}px`
+          }}>{newStr[2]} {newStr[3]} {newStr[4]} {newStr[5]} {newStr[6]}</span>
             </div>
           </>
 
         // default message in chat
         default:
-          return <div className="outputChatlog">{str}</div>;
+          return <div className="outputChatlog" style={{
+            fontSize: `${fontsize}px`
+          }}>{str}</div>;
       }
     });
   }
@@ -101,6 +132,10 @@ export default function HandleChatlog() {
       />
       <div id="chatlog">{formatChatlog(chatlog)}</div>
       <button onClick={handleSaveClick}>Download Image</button>
+
+      <button onClick={() => {setFontsize(fontsize + 1)}}>+</button>
+      <button onClick={() => {setFontsize(fontsize - 1)}}>-</button>
+      {fontsize}px
     </div>
   );
 }
